0da697ceeb6727eb0a1a407f1f538cee265cd323
JiuYo <39853731+JiuYo@users.noreply.github.com>
2019-07-03 23:57:23 +0800

    明细的显示

--- view/js/stock/updatestock.js
+++ view/js/stock/updatestock.js
@@ -5,6 +5,8 @@ define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/s
 	var ad = {};
   updatestock.init = function (page) {
   	mui.init();
+		mui.previewImage();
+		mui(".mui-scroll-wrapper").scroll();
 	};
 	mui.plusReady(function(){
 		var currentView = plus.webview.currentWebview();
@@ -37,50 +39,37 @@ define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/s
 		var params = {};
 		params.pid = id;
 		stock.getOutDetailsByPId(params,function(data){
-			console.log(JSON.stringify(data));
 			$('.status-list').append(updatestock.progress(data));
-		},function(errorinfo){
-			
+		},function(errorinfo){	
 			mui.alert("请求失败!" + systemutil.parsestr(errorinfo));
 		});
 	}
 	
 	updatestock.progress = function (data){
-    	if(systemutil.isNotBlank(data)){
+		console.log(JSON.stringify(data));
+    	if(systemutil.isBlank(data)){
     		var html = '';
     		html += '<p style="text-align:center;">没有查询到数据!</p>'
     		return html;
     	}else{
 			var html = '';
-			$.each(data, function(i, obj) {
+			// $.each(data, function(i, obj) {
+			for(var i =0;i<data.length;i++){
+				var obj = data[i];
 				html +='<li>'
              +'<div class="status-content-before">'
-             +'<span class="fl">'+obj.departName+' : '+obj.operator+' '+systemutil.parsestr(obj.optName)+'</span>'
-							 +'<span class="fr">'+common.formatSolrTime(obj.operationTime)+'</span>'
+             +'<span class="fl">'+ad.pname+' : '+obj.outqty+'/'+ad.unit+'</span>'
+							 +'<span class="fr">'+obj.outtime+'</span>'
              +'</div>'
              +'<div class="status-time-before">'
              +'<div class="mui-content-padded">'
              +'<ul>'
-             if(obj.attachment){
-             	 var imgList = obj.attachment.split(',');
-             	 var parentImgPath = common.getServerurl();
-                 $.each(imgList, function(i,obj) {
-                 	if(systemutil.isNotBlank(obj)){
-	                 	var path = parentImgPath+obj;
-	                 	html+='<li>'
-	                  +'<img src="'+path+'" data-preview-src data-preview-group="2">'
-	                  +'</li>'
-                  }
-                 });
-             }
              html+='</ul>'
              +'</div>'
              +'</div>'
-             +'<div class="status-line"> 附言:'
-             + (!obj.remark ? "无" : obj.remark)
-             +'</div>'
              +'</li>'
-			})
+			// })
+			}
 			return html;
     	}
     		
@@ -93,14 +82,9 @@ define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/s
 		var stockVal = $("#stock").html();
 		var currentQty = $("#currentQty").val();
 		var saleVal = $("#sale").html();
-		// var stockVal = ad.stock;
-		// var currentQty = $("#currentQty").val();
-		// var saleVal = ad.sale;
 		stockVal = parseInt(stockVal.trim());
 		currentQty = parseInt(currentQty.trim());
 		saleVal = parseInt(saleVal.trim());
-		// var currSale = eval(stockVal-currentQty);
-		// var newSaleVal = eval(saleVal+currSale);
 		// 修改货品表数据；stock：之前-当日；sale：之前+当日
 		var paramss = {};
 		paramss.ID = id;
@@ -115,8 +99,7 @@ define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/s
 		stock.updatestockById(paramss,function(data){
 				// 保存售出明细；pid：货品id；date：当前日期；outqty：当日卖出
 				stock.insertStockDetailes(param,function(data){
-					$('#layui-m-layer0').hide();
-					
+					$('#layui-m-layer0').hide();			
 					// 返回上一页、刷新列表、关闭当前页
 					setTimeout(function(){
 						var currView = plus.webview.currentWebview();
@@ -132,10 +115,22 @@ define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/s
 		},function(errorinfo){
 			$('#layui-m-layer0').hide();
 			mui.alert("请求失败!" + systemutil.parsestr(errorinfo));
-		}); 
-		
-		
+		}); 	
 	})
+	
+		//安卓滚动到可视区域
+	window.addEventListener("resize", function() {
+			var u = navigator.userAgent;
+			if(u.indexOf('Android') > -1 || u.indexOf('Adr') > -1) {
+				if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+	         window.setTimeout(function() {
+	            document.activeElement.scrollIntoView(false);//scrollIntoViewIfNeeded()  
+	         },0);
+	      }
+			}else{}
+	    
+	})
+	
 
   return updatestock;
 });
