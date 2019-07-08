5e89ef664380c7df61631a9af8c9577e632eaaa3
JiuYo <39853731+JiuYo@users.noreply.github.com>
2019-07-08 22:26:04 +0800

    修改JS

---
+++ view/js/stock/activate.js
@@ -0,0 +1,92 @@
+define(['jquery',"mui","common","service/stock/stock","model/UserModel",'utils/systemutil','vue'], function ($,mui,common,stock,umodel,systemutil,vue) {　　　　
+  var activate = {};
+	var params = {};
+  params.page = '';
+  activate.init = function (page) {
+  	mui.init();
+	};
+	mui.plusReady(function(){
+		// activate.getStockList();
+	});
+		//uuid 的加密解密方法调用
+	function getUuid() {
+		//随机获得一串字符串
+		var b = Math.random().toString(36).substr(2);
+		//把唯一的uuid赋值给输入框显示到界面
+		$("#auuid").val(b);
+	/* 	//加密方法
+		var c = compileStr(b);
+		alert(c)
+		//解密方法
+		var d = uncompileStr(c);
+		alert(d); */
+	}
+	//写点无用的句子
+	
+	
+	//判断验证激活方法   两个参数,一个是随机参数,一个是加密后的参数 
+	function  activate(){
+		var yuanma = $("#auuid").val();
+		var jiamima =$("#mima").val();
+		//把加密码转换成源码
+		var zyuanma = uncompileStr(jiamima);
+		alert(1)
+		alert(zyuanma)
+		//判断转换后的源码是否与自动生成的源码一致,然后进行判断
+		if(zyuanma == yuanma){
+			//调用后台方法,把后台字段的时间限制去掉
+			alert("激活成功了！")
+		}else{
+			alert("激活码错误，请重新输入！！！")
+		}
+	}
+	
+	//自动算码方法
+	$("#suanma").on('tap',function(){
+		var yuanma = $("#auuid").val();
+		var sma = compileStr(yuanma);
+		$("#suanma").val(sma);
+	})
+	function suanma(){
+		var yuanma = $("#auuid").val();
+		var sma = compileStr(yuanma);
+		$("#suanma").val(sma);
+		}
+	//对随机字符串进行加密  
+	function compileStr(code) {
+		var c = String.fromCharCode(code.charCodeAt(0) + code.length);
+		for (var i = 1; i < code.length; i++) {
+			c += String.fromCharCode(code.charCodeAt(i) + code.charCodeAt(i - 1));
+		}
+		return escape(c);
+	}
+	
+	//对随机字符串进行解密  
+	function uncompileStr(code) { 
+		code = unescape(code);
+		var c = String.fromCharCode(code.charCodeAt(0) - code.length);
+		for (var i = 1; i < code.length; i++) {
+			c += String.fromCharCode(code.charCodeAt(i) - c.charCodeAt(i - 1));
+		}
+		return c;
+	}
+
+//判断验证激活方法   两个参数,一个是随机参数,一个是加密后的参数 
+	$("#buttonActivate").on('tap',function () {
+		var yuanma = $("#auuid").val();
+		var jiamima =$("#mima").val();
+		//把加密码转换成源码
+		var zyuanma = uncompileStr(jiamima);
+		//判断转换后的源码是否与自动生成的源码一致,然后进行判断
+		if(zyuanma == yuanma){
+			//调用后台方法,把后台字段的时间限制去掉
+			alert("激活成功了！")
+		}else{
+			alert("激活码错误，请重新输入！！！")
+		}
+	});
+	
+  return activate;
+});
+	
+	
